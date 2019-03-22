767cff98d2968a8b7de4bf662445010966ecc125
tsgouros <tsgouros@gmail.com>
2019-03-22 19:05:23 -0400

    mutual() seems to work

--- src/truth.table.r
+++ src/truth.table.r
@@ -37,7 +37,7 @@ format.outcome <- function(p) {
 }
 
 print.outcome <- function(p) {
-    if (class(p) != "outcome") { exit(); }
+    if (class(p) != "outcome") { stop(); }
     cat(format.outcome(p), "\n");
 }
 
@@ -48,7 +48,7 @@ truth.table <- function(inputs, outcomes) {
 }
 
 ## Here's an AND gate
-samp.tt <- truth.table(inputs=data.frame(in1=c(0,0,1,1),in2=c(0,1,0,1)),
+AND.tt <- truth.table(inputs=data.frame(in1=c(0,0,1,1),in2=c(0,1,0,1)),
                        outcomes=list(outcome(out=c(0),prob=c(1)),
                                      outcome(out=c(0),prob=c(1)),
                                      outcome(out=c(0),prob=c(1)),
@@ -56,14 +56,24 @@ samp.tt <- truth.table(inputs=data.frame(in1=c(0,0,1,1),in2=c(0,1,0,1)),
 
 ## Here's a sucky AND gate, that has trouble getting the right answer
 ## for a couple of combinations.
-samp2.tt <- truth.table(inputs=data.frame(in1=c(0,0,1,1),in2=c(0,1,0,1)),
+AND2.tt <- truth.table(inputs=data.frame(in1=c(0,0,1,1),in2=c(0,1,0,1)),
                         outcomes=list(outcome(out=c(0),prob=c(1)),
                                       outcome(out=c(0),prob=c(1)),
                                       outcome(out=c(0,1),prob=c(0.9,0.1)),
                                       outcome(out=c(0,1),prob=c(0.5,0.5))));
 
+## This is sort of like Shuki Bruck's pswitch.  The output is a random choice
+## among the inputs.
+PSWITCH.tt <- truth.table(inputs=data.frame(in1=c(0,0,1,1),in2=c(0,1,0,1)),
+                        outcomes=list(outcome(out=c(0),prob=c(1)),
+                                      outcome(out=c(0,1),prob=c(0.5,0.5)),
+                                      outcome(out=c(0,1),prob=c(0.5,0.5)),
+                                      outcome(out=c(1),prob=c(1))));
+
+
+
 print.truth.table <- function(tt) {
-    if (class(tt) != "truth.table") { exit(); }
+    if (class(tt) != "truth.table") { stop(); }
 
     for (j in 1:dim(tt$inputs)[2]) {
         cat(j, " ");
@@ -123,7 +133,7 @@ calc.prob.table <- function(tt, input.probs) {
 ##      c(".000" ".100" ".010" ".110" ".001" ".101" ".011" ".111")
 ## Note that this always includes an extra column for the output, the last
 ## column on the right.
-permute <- function(level, cols) {
+permute.recurse <- function(level, cols) {
 
     suffixes <- c("0", "1");
 
@@ -132,7 +142,7 @@ permute <- function(level, cols) {
     if (length(cols) == 0) {
         output <- suffixes;
     } else {
-        temp <- permute(level + 1, cols[-1]);
+        temp <- permute.recurse(level + 1, cols[-1]);
 
         for (string in temp) {
 
@@ -151,38 +161,46 @@ permute <- function(level, cols) {
     return(output);
 }
 
-## With definitions of truth table in place, now we can create
-## functions to calculate various definitions of mutual information.
-## TT is the input truth.table object, INPUT.PROBS is a list
-## containing the input probablity distributions, and COLS refers to
-## the columns we're looking at.  For now we're using the same
-## input.probs for all the inputs, and the cols are specified with an
-## index number.  Use zero to reference the output.  So cols=c(1,0)
-## will give the mutual information between the first input and the
-## output, and cols=c(1,2,0) will give the three-way mutual
-## information.  I hope.
-mutual <- function(tt, input.probs, cols) {
+permute <- function(cols) {
+    return(permute.recurse(1, cols));
+}
 
+## With definitions of truth table in place, now we can create functions to
+## calculate various definitions of mutual information.  TT is the input
+## truth.table object, INPUT.PROBS is a list containing the input probablity
+## distributions, and COLS refers to the columns we're looking at.  For now
+## we're using the same input.probs for all the inputs, and the cols are
+## specified with an index number.  Use zero to reference the output and
+## negative numbers to ignore columns.  So cols=c(1,-2) will give the mutual
+## information between the first input and the output, and cols=c(1,2) will
+## give the three-way mutual information between two inputs and the output.
+calc.probs <- function(tt, input.probs, cols) {
+
+    ## Do some checking.
+    if (length(cols) != dim(tt$inputs)[2]) {
+        cat("Wrong number of column indicators for this truth table.\n");
+        stop();
+    }
+
+    ## Get the probability table for this truth table, assuming the
+    ## given input probabilities.
     cpt <- calc.prob.table(tt, input.probs);
-    permutations <- permute(1, cols);
 
-    print(cpt);
+    ## Now generate regular expressions to pull the relevant
+    ## combinations out of the probability table.
+    permutations <- permute(cols);
 
     out <- list();
 
     ## Generate all the possible values of the symbols identified by cols.
     for (p in permutations) {
 
-        cat("checking out: ", p, "\n");
-
         found <- grep(p, names(cpt));
-        print(found);
+
         s <- 0;
         for (f in found) {
             s <- s + cpt[[names(cpt)[f]]];
         }
-
-        print(s);
 
         out[[p]] <- s;
     }
@@ -190,13 +208,57 @@ mutual <- function(tt, input.probs, cols) {
     return(out);
 }
 
+mutual <- function(tt, input.probs, cols) {
+
+    probs <- calc.probs(tt, input.probs, cols);
+
+    ## Pick up the probability of the output symbols.  Multiplying the cols
+    ## vector by zero ensures none of the result will match the level.
+    output.prob <- calc.probs(tt, input.probs, cols*0)
+
+    ## Change the names of output.prob to make them easier to use.
+    for (i in 1:length(output.prob)) {
+        nchars <- nchar(names(output.prob)[i]);
+        names(output.prob)[i] <- substr(names(output.prob)[i], nchars, nchars);
+    }
+
+    ## We are using the KL divergence to stand for mutual information.  So we
+    ## need the product of the input probabilities corresponding to the
+    ## elements in the string for each probability.
+    out <- 0;
+    for (i in 1:length(probs)) {
+        joint.prob <- probs[[ names(probs)[i] ]];
+
+        ## Parse names(probs)[i] to find the probabilities of the components
+        ## of the joint probability.
+        chars <- strsplit(names(probs)[i], c())[[ 1 ]];
+
+        prod.prob <- 1;
+        for (j in 1:length(cols)) {
+            if (cols[j] > 0) {
+                prod.prob <- prod.prob * input.probs[[ chars[j] ]];
+            }
+        }
+
+        ## Also get the output character, which is the next one.
+        prod.prob <- prod.prob * output.prob[[ chars[length(cols)+1] ]];
+
+        if (probs[[i]] != 0) {
+            out <- out + probs[[i]] * log(probs[[i]] / prod.prob, base=2);
+        }
+    }
+    return(out);
+}
+
+
 
 ## The simple information content of an array of probabilities.
-H <- function(p) {
+H <- function(ps) {
     sum <- 0;
-    for (i in 1:length(p)) {
-        if (p[i] != 0) {
-            sum <- sum - p[i] * log(p[i], base=2);
+    for (p in ps) {
+        pnum <- as.numeric(p);
+        if (pnum != 0) {
+            sum <- sum - pnum * log(pnum, base=2);
         }
     }
     return(sum);
